fbddba8df6dc0bf0a9bc38cd70317edfdadc5b5b
Madhuri Doguparthi <mdoguparthi@corp.logitech.com>
2025-03-01 06:57:40 +0530

    Assignment1

--- src/Assignments/Assignment1_DataType.java
+++ src/Assignments/Assignment1_DataType.java
@@ -1,5 +1,108 @@
 package Assignments;
 
 public class Assignment1_DataType {
-
+	
+	public static void main(String[]  args) {
+		
+		//Temperature of a city in degrees Celsius: 25.5
+		float temperature = 25.5f;
+		System.out.println("Temperature of a city in degrees Celsius" +temperature);
+		
+		//Whether a customer has placed an order: true or false
+		boolean CustomerPlacedOrder = true;
+		boolean customerNotPlacedOrder = false;
+		System.out.println("Whether a customer has placed an order :" +CustomerPlacedOrder);
+		System.out.println("Whether a customer has placed an order :" +customerNotPlacedOrder);
+		
+		//Person's phone number: "123-456-7890"
+		
+		long PhoneNUmber = 1234567890l;
+		System.out.println("Person's phone number:" +PhoneNUmber);
+		
+		//Amount of money in a customer's bank account: 1000.50
+		
+		double amount = 1000.50;
+		System.out.println("Amount of money in a customer's bank account: " +amount);
+		
+		//Person's email address: "john.doe@example.com"
+		String email = "john.doe@example.com";
+		System.out.println("Person's email address:" +email);
+		
+		//Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+		
+		double latitude = 37.7749;
+		double longitude = -122.4194;
+		System.out.print("Coordinates of a location (latitude, longitude):" +latitude);
+		System.out.println(","+longitude);
+		
+		//Person's marital status: true or false
+		boolean status = true;
+		boolean statusNo = false;
+		System.out.print("Person's marital status :" +status);
+		System.out.println(" or "+statusNo);
+		
+		//Person's occupation: "Software Engineer"
+		String occupation = "software Engineer";
+		System.out.println("Person's occupation: " +occupation);
+		
+		//Person's favourite colour: "Blue"
+		String color = "Blue";
+		System.out.println("Person's favourite color " +color);
+		
+		//10.Current year: 2023
+		int year = 2023;
+		System.out.println("Current year: " +year);
+		
+		//11.Number of followers on a social media platform: 1,000,000
+		int Number = 1000000;
+		System.out.println("Number of followers on a social media platform: " +Number);
+		
+		//12.Rating of a movie: 7.5
+		float Movie = 7.5f;
+		System.out.println("Rating of a movie: " +Movie);
+		
+		//13.Person's blood type: 'A'
+		char Type ='A';
+		System.out.println("Person's blood type: "+Type);
+		
+		//14.Title of a book: "To Kill a Mockingbird"
+		String title = "To Kill a Mockingbird";
+		System.out.println("Title of a book: " +title);
+		
+		//15.Number of employees in a company: 500
+		int NoOfEmployees = 500;
+		System.out.println("Number of employees in a company: "+NoOfEmployees);
+		
+		//16.Time of an event: 2:30 PM
+		String Time = "2:30 PM";
+		System.out.println("Time of an event: "+Time);
+		
+		//17.Name of a country: "United States"
+		String Country = "United States";
+		System.out.println("Name of a country: "+Country);
+		
+		//18.Person's eye color: "Brown"
+		String color1 = "Brown";
+		System.out.println("Person's eye color: "+color1);
+		
+		//19.Person's birthplace: "New York City"
+		String birthPlace = "New York City";
+		System.out.println("Person's birthplace: "+birthPlace);
+		
+		//20.Distance between two cities: 200.5
+		float Distance = 200.5f;
+		System.out.println("Distance between two cities: "+Distance);		
+		
+		
+		
+		
+	  
+		
+		
+		
+		
+				
+				
+		
+	}
 }
